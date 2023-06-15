b460f4488871d04cd93a09d7def715516d6a1e4b
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2023-06-15 00:42:52 -0300

    estrutura inicial

---
+++ client/src/App.vue
@@ -0,0 +1,72 @@
+<script>
+
+export default {
+  data() {
+    return {
+      inputValue: '',
+      searchData: null
+    }
+  },
+  methods: {
+
+    podFetch() {
+      //
+      fetch(`https://spotify23.p.rapidapi.com/search/?q=${this.inputValue}&type=episodes&limit=100`, {
+        headers: {
+          'X-RapidAPI-Key': '0a8025daa0msh1827d8b09fb2a34p1d1a3bjsn017c5eee4962',
+          'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          this.searchData = data
+        })
+
+    }
+
+  }
+}
+
+</script>
+
+<template>
+  <nav>
+    <section>
+      <input type="text" placeholder="Pesquise Podcasts, Episódios e Artistas" v-model="inputValue" @keyup.enter="podFetch">
+      <ion-icon id="searchIcon" name="search-outline"></ion-icon>
+    </section>
+  </nav>
+  <main>
+    <div class="content" v-if="searchData">
+      <ul>
+        <li v-for="episode in searchData.episodes.items" :key="episode.data">
+          <img :src="episode.data.coverArt.sources[1].url" :alt="episode.data.name" draggable="false">
+          <section>
+            <h3>{{ episode.data.name }}</h3>
+          </section>
+        </li>
+      </ul>
+    </div>
+    <div class="player">
+      <img src="/album-model.jpg" alt="">
+      <section class="ep-info">
+        <h3>Podcast Episode</h3>
+        <h4>Artista</h4>
+      </section>
+      <section class="audio-player">
+        <section class="audio-buttons">
+          <ion-icon name="play-skip-back"></ion-icon>
+          <ion-icon name="play"></ion-icon>
+          <ion-icon name="play-skip-forward"></ion-icon>
+        </section>
+        <section>
+          <progress value="100"></progress>
+        </section>
+      </section>
+
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
