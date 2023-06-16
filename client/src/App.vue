eec0e3041bb64a0de94a166b9e3e020032e4dd18
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2023-06-15 22:28:09 -0300

    buscando audio do episódio

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,6 +7,7 @@ export default {
       searchData: null,
       episodeArt: '/default-art.png',
       episodeTitle: 'Selecione um Episódio',
+      episodeAudio: ''
     }
   },
   methods: {
@@ -14,7 +15,7 @@ export default {
     // funções do componente
 
     podFetch() {
-  
+
       fetch(`https://spotify23.p.rapidapi.com/search/?q=${this.inputValue}&type=episodes&limit=100`, {
         headers: {
           'X-RapidAPI-Key': '0a8025daa0msh1827d8b09fb2a34p1d1a3bjsn017c5eee4962',
@@ -29,15 +30,27 @@ export default {
 
     },
 
-    cowsay(event){
+    cowsay(event) {
 
       const newEpisodeTitle = event.target.parentNode.childNodes[1].childNodes[0].innerText
       const episodeID = event.target.parentNode.childNodes[1].childNodes[1].innerText.split(":")
       this.episodeArt = event.target.src
       this.episodeTitle = newEpisodeTitle
-      
+
       // Episode ID
       console.log(episodeID[2])
+
+      fetch(`https://spotify23.p.rapidapi.com/episode_sound/?id=${episodeID[2]}`, {
+        headers: {
+          'X-RapidAPI-Key': '0a8025daa0msh1827d8b09fb2a34p1d1a3bjsn017c5eee4962',
+          'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          this.episodeAudio = data.url[1]
+        })
 
     }
 
@@ -57,7 +70,8 @@ export default {
     <div class="content" v-if="searchData">
       <ul>
         <li v-for="episode in searchData.episodes.items" :key="episode.data">
-          <img :src="episode.data.coverArt.sources[1].url" :alt="episode.data.name" @click="cowsay($event)" draggable="false">
+          <img :src="episode.data.coverArt.sources[1].url" :alt="episode.data.name" @click="cowsay($event)"
+            draggable="false">
           <section>
             <h3>{{ episode.data.name }}</h3>
             <p>{{ episode.data.uri }}</p>
@@ -68,17 +82,12 @@ export default {
     <div class="player">
       <img :src="episodeArt" alt="Arte do Episódio" draggable="false">
       <section class="ep-info">
-        <h3>{{episodeTitle}}</h3>
+        <h3>{{ episodeTitle }}</h3>
       </section>
       <br>
       <section class="audio-player">
-        <section class="audio-buttons">
-          <ion-icon name="play-skip-back"></ion-icon>
-          <ion-icon name="play"></ion-icon>
-          <ion-icon name="play-skip-forward"></ion-icon>
-        </section>
         <section>
-          <progress value="100"></progress>
+          <audio :src="episodeAudio" preload="auto" controls></audio>
         </section>
       </section>
 
