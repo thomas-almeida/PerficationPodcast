ed6914447196fc017970d9b52f0dbf0774629ac4
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2023-06-15 21:04:11 -0300

    capturando titulo e art dos eps

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,13 +4,17 @@ export default {
   data() {
     return {
       inputValue: '',
-      searchData: null
+      searchData: null,
+      episodeArt: '/album-model.jpg',
+      episodeTitle: 'Selecione um Episódio',
     }
   },
   methods: {
 
+    // funções do componente
+
     podFetch() {
-      //
+  
       fetch(`https://spotify23.p.rapidapi.com/search/?q=${this.inputValue}&type=episodes&limit=100`, {
         headers: {
           'X-RapidAPI-Key': '0a8025daa0msh1827d8b09fb2a34p1d1a3bjsn017c5eee4962',
@@ -23,6 +27,13 @@ export default {
           this.searchData = data
         })
 
+    },
+
+    cowsay(event){
+      this.episodeArt = event.target.src
+      let newEpisodeTitle = event.target.parentNode.childNodes[1].childNodes[0].innerText
+      this.episodeTitle = newEpisodeTitle
+
     }
 
   }
@@ -33,7 +44,7 @@ export default {
 <template>
   <nav>
     <section>
-      <input type="text" placeholder="Pesquise Podcasts, Episódios e Artistas" v-model="inputValue" @keyup.enter="podFetch">
+      <input type="text" placeholder="..." v-model="inputValue" @keyup.enter="podFetch">
       <ion-icon id="searchIcon" name="search-outline"></ion-icon>
     </section>
   </nav>
@@ -41,7 +52,7 @@ export default {
     <div class="content" v-if="searchData">
       <ul>
         <li v-for="episode in searchData.episodes.items" :key="episode.data">
-          <img :src="episode.data.coverArt.sources[1].url" :alt="episode.data.name" draggable="false">
+          <img :src="episode.data.coverArt.sources[1].url" :alt="episode.data.name" @click="cowsay($event)" draggable="false">
           <section>
             <h3>{{ episode.data.name }}</h3>
           </section>
@@ -49,11 +60,11 @@ export default {
       </ul>
     </div>
     <div class="player">
-      <img src="/album-model.jpg" alt="">
+      <img :src="episodeArt" alt="Arte do Episódio" draggable="false">
       <section class="ep-info">
-        <h3>Podcast Episode</h3>
-        <h4>Artista</h4>
+        <h3>{{episodeTitle}}</h3>
       </section>
+      <br>
       <section class="audio-player">
         <section class="audio-buttons">
           <ion-icon name="play-skip-back"></ion-icon>
